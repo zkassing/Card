f9240a77365fbe7db9b2d053a31e19ad83b0e91c
zkassing <kassing@live.cn>
2017-01-11 22:40:47 +0800

    功能重置

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,28 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var path = require('path');
+var multiparty = require('multiparty');
+
 var date;
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 router.post('/make', function (req, res, next) {
+    var form = new multiparty.Form();
+    form.encoding = "uft-8";
+    form.uploadDir = "/files/";
+    form.parse(req,function (err,fields,files) {
+        // console.log(fields,files);
+        console.log(files);
+        // console.log(files);
+
+        
+
+    })
+    
+    
+    
     var to = req.body.to;
     var from = req.body.from;
     var content = req.body.content;
